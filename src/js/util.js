8ac7b5433d07800a90d046eb26792fe47bac8773
v-almonacid <ealmonacid@gmail.com>
2018-10-15 18:58:48 +0200

    ipfs/ adding config file and simple utility functions

---
+++ src/js/util.js
@@ -0,0 +1,18 @@
+const bs58 = require('bs58')
+
+const util = {
+  
+  ipfsHashToBytes: function(hash) {
+    let bytes = bs58.decode(hash);
+    // first 2 bytes are hash function identifier and length
+    let multiHashIdLen = 2;
+    // remove the multihash hash id
+    return '0x' + bytes.slice(multiHashIdLen, bytes.length);
+  },
+
+  // TODO
+  bytesToIpfsHash: function() {}
+
+}
+
+export default util
