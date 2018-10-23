94adc25f29365ac363f54553ae3bb72906ff49ec
v-almonacid <ealmonacid@gmail.com>
2018-10-23 18:04:32 +0200

    fix: deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,6 @@ var Legacy = artifacts.require("./Legacy.sol");
 module.exports = function (deployer, network, accounts) {
   var tPoL = 90;
   var beneficiaries = ['0x343295b49522cfc38af517c58ebb78565c42ed95'];
-  var messageAdds = '12347896;'
+  var messageAdds = ['0x7d5a99f603f231d53a4f39d1521f98d2e8bb279cf29bebfd0687dc98458e7f89'];
   deployer.deploy(Legacy, tPoL, beneficiaries, messageAdds);
 };
